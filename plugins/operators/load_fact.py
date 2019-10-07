38b365af719ca70e1c06b2b69f8d177cb6f6466a
Jacob Dodd <jacobdodd94@gmail.com>
2019-10-06 21:32:30 -0700

    Update load_fact.py

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -35,8 +35,6 @@ class LoadFactOperator(BaseOperator):
 
     def execute(self, context):
         self.log.info("Making Connections to Redshift..")
-        aws_hook = AwsHook(self.aws_credentials_id)
-        credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         # moving data over from redshift
         insert_sql = SqlQueries.songplay_table_insert
